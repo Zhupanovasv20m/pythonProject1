88d6887a5bd64f2d0337fbd64b7835f8123f29e7
Alice <zhupanova2017sv@mail.ru>
2023-11-28 13:51:17 +0300

    Done

---
+++ lesson1/task1.py
@@ -0,0 +1,12 @@
+from zeep import Client, Settings
+import yaml
+
+with open('config.yaml') as f:
+    data = yaml.safe_load(f)
+
+
+def check_word(word):
+    url = data['url']
+    settings = Settings(strict=False)
+    client = Client(wsdl=url, settings=settings)
+    return client.service.checkText(word)[0]['s']
